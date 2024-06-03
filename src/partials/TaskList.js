56b609166dbc8af7f1bd24c33aa12518cb8c17f2
Julian Hernandez <hernandezjuliang44@gmail.com>
2024-06-03 09:53:08 -0600

    css

--- src/partials/TaskList.js
+++ src/partials/TaskList.js
@@ -6,11 +6,23 @@
 *         -- As soon as you save any changes on your file, it will show on your internet tab.*/
 
 import './TaskList.css';
+import tasks from '../test.json';
+import TaskDetail from './TaskDetail';
 
 function TaskList() {
   return (
     <div className="taskList">
-        <h1>TaskList</h1>
+        <h1>Task List</h1>
+        <ul className="taskUl">
+        {tasks.map((task, index) => (
+          <div key={task} className="taskDiv">
+            <button onClick={TaskDetail}>
+              <p className="pTitle">{task.What}</p>
+              <p>When: {new Date(task.When).toLocaleString()}</p>
+            </button>
+          </div> 
+        ))}
+        </ul>
     </div>
   );
 }
