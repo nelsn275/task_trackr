bb1a064fab5976a19800bf4697b77479ceebed89
booth0 <122390745+booth0@users.noreply.github.com>
2024-06-17 09:09:11 -0600

    Routing

--- src/App.js
+++ src/App.js
@@ -3,28 +3,31 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Showcase from './pages/Showcase';
 import logo from './logo.svg';
 import './App.css';
 import PartialShowcase from './partials/PartialShowcase';
+import Header from './partials/Header';
 
 function App() {
   return (
 
     <div className="App">
 
-      <PartialShowcase />
-
+      {/* <PartialShowcase /> */}
+      <Header />
       <Router>
         <div>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/about" element={<About />} />
+            <Route path="/showcase" element={<Showcase />} />
           </Routes>
         </div>
       </Router>
 
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -37,7 +40,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
