2daa552f57e1855cd5297c6be8e9bb6a9d7edb0b
mtmenji <mtmenji@gmail.com>
2024-06-17 09:59:49 -0600

    Created register and sign in pages.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+
+
+//Partial Imports
+import Header from './partials/Header';
+
+//Page Imports
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import About from './pages/About';
@@ -10,26 +18,31 @@ import ThankYou from './pages/ThankYou';
 import './App.css';
 import PartialShowcase from './partials/PartialShowcase';
 import Header from './partials/Header';
+import Register from './pages/Register';
+import SignIn from './pages/SignIn';
+
 
 function App() {
   return (
 
     <div className="App">
 
-      <PartialShowcase />
-
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/partial" element={<PartialShowcase />} />
-            <Route path="/dashboard" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/showcase" element={<Showcase />} />
-            <Route path="/thankyou" element={<ThankYou/>} />
-          </Routes>
-        </div>
-      </Router>
+      {/* <PartialShowcase /> */}
+      <Header />
+      <AuthProvider>
+        <Router>
+          <div>
+            <Routes>
+              <Route path="/dashboard" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/showcase" element={<Showcase />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/signin" element={<SignIn />} />
+            </Routes>
+          </div>
+        </Router>
+      </AuthProvider>
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
