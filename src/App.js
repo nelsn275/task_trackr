8833bf3f589a14904237be6ceab2cae47cad79ef
Julian Hernandez <hernandezjuliang44@gmail.com>
2024-06-24 09:25:55 -0600

    Fixed changes

--- src/App.js
+++ src/App.js
@@ -3,40 +3,20 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 
-
-//Partial Imports
-import Header from './partials/Header';
-import Footer from './partials/Footer'
-
 //Page Imports
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import About from './pages/About';
-import Showcase from './pages/Showcase';
-import Showcase from './pages/Showcase';
-import logo from './logo.svg';
 import ThankYou from './pages/ThankYou';
-import './App.css';
-import PartialShowcase from './partials/PartialShowcase';
-import Header from './partials/Header';
 import Register from './pages/Register';
 import SignIn from './pages/SignIn';
-
 import Showcase from './pages/Showcase';
-import logo from './logo.svg';
-import ThankYou from './pages/ThankYou';
-import ThankYou from './pages/ThankYou';
-import './App.css';
-import PartialShowcase from './partials/PartialShowcase';
-import Header from './partials/Header';
 
 function App() {
   return (
 
     <div className="App">
 
-      {/* <PartialShowcase /> */}
-      <Header />
       <AuthProvider>
         <Router>
           <div>
@@ -47,11 +27,11 @@ function App() {
               <Route path="/showcase" element={<Showcase />} />
               <Route path="/register" element={<Register />} />
               <Route path="/signin" element={<SignIn />} />
+              <Route path="/thankyou" element={<ThankYou />} />
             </Routes>
           </div>
         </Router>
       </AuthProvider>
-    <Footer />
     </div>
   );
 }
