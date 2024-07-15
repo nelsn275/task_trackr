bc194209072cf03dfbbd491e02c123246e9d5e7a
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2024-07-15 10:18:34 -0600

    tasks save to firestore database

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app)
+const db = getFirestore(app);
 const auth = getAuth(app);
 
-export {app, auth, db};
+export { app, auth, db };
